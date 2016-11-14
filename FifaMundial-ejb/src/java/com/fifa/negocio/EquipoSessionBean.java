46baaade5464144977c609bdaef34e90a09fac45
Alexia <>
2016-11-13 22:55:06 -0300

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	src/com/game/controllers/MainController.java

--- FifaMundial-ejb/src/java/com/fifa/negocio/EquipoSessionBean.java
+++ FifaMundial-ejb/src/java/com/fifa/negocio/EquipoSessionBean.java
@@ -14,10 +14,10 @@ import com.fifa.datos.Equipo;
 import com.fifa.datos.Zona;
 import com.fifa.datos.Pais;
 import com.fifa.datos.Mundial;
+import java.util.ArrayList;
 import java.util.Arrays;
 import javax.persistence.Query;
 import javax.servlet.jsp.tagext.TryCatchFinally;
-
 
 /**
  *
@@ -29,98 +29,87 @@ public class EquipoSessionBean {
 
     @PersistenceContext(unitName = "FifaMundial-ejbPU")
     private EntityManager em;
-   
-  
 
-    
-    public List<Equipo> obtenerEquipo()
-     {
-         try {
-             javax.persistence.Query q= em.createNamedQuery("Equipo.findAll");
-             return q.getResultList();
-         } catch (Exception e) {
-             return null;
-         }
-     }
-    
-    public List<Equipo> obtenerEquipoNombre(String nombre)
-     {
-         try {
-             javax.persistence.Query q= em.createNamedQuery("Equipo.findByNombre");
-             q.setParameter("nombre", "%" + nombre);
-             return q.getResultList();
-         } catch (Exception e) {
-             return null;
-         }
-     }
-    
-    public Equipo obtenerEquipoId(int idEquipo)
-     {
-         try {
+    public List<Equipo> obtenerEquipo() {
+        try {
+            javax.persistence.Query q = em.createNamedQuery("Equipo.findAll");
+            return q.getResultList();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public List<Equipo> obtenerEquipoNombre(String nombre) {
+        try {
+            javax.persistence.Query q = em.createNamedQuery("Equipo.findByNombre");
+            q.setParameter("nombre", "%" + nombre);
+            return q.getResultList();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public Equipo obtenerEquipoId(int idEquipo) {
+        try {
             em.getEntityManagerFactory().getCache().evict(Equipo.class);
             Equipo a = em.find(Equipo.class, idEquipo);
             return a;
         } catch (Exception e) {
-             return null;
-         }
-     }
-    
+            return null;
+        }
+    }
+
     public boolean borrarEquipo(int idEquipo) {
         try {
             em.getEntityManagerFactory().getCache().evict(Equipo.class);
             Equipo p = em.find(Equipo.class, idEquipo);
-            em.remove(p); 
+            em.remove(p);
             return true;
         } catch (Exception e) {
             return false;
         }
     }
 
-
-    public boolean agregarEquipo( Pais paisidPais, Zona zonaidZona, Mundial mundialidMundial) {
+    public boolean agregarEquipo(Pais paisidPais, Zona zonaidZona, Mundial mundialidMundial) {
         try {
-            if (Validator(mundialidMundial,paisidPais)==false){
-            Equipo p = new Equipo();
-            p.setPaisidPais(paisidPais);
-            p.setZonaidZona(zonaidZona);
-            p.setMundialidMundial(mundialidMundial);
-            em.persist(p);
-            em.flush();
-            return true;
+            if (Validator(mundialidMundial, paisidPais) == false) {
+                Equipo p = new Equipo();
+                p.setPaisidPais(paisidPais);
+                p.setZonaidZona(zonaidZona);
+                p.setMundialidMundial(mundialidMundial);
+                em.persist(p);
+                em.flush();
+                return true;
             }
         } catch (Exception e) {
-        
+
         }
-            return false;
+        return false;
     }
-     
-        public Boolean  Validator (Mundial idMundial, Pais idPais){
-     String p1=null;
-     String consulta;
+
+    public Boolean Validator(Mundial idMundial, Pais idPais) {
+        String p1 = null;
+        String consulta;
         try {
             consulta = "FROM Equipo p WHERE p.mundialidMundial= ?1 and p.paisidpais=2? ";
             Query query = em.createQuery(consulta);
             query.setParameter(1, idMundial);
             query.setParameter(2, idPais);
-            
-                   
-        List <Pais> Listpa = query.getResultList();
+
+            List<Pais> Listpa = query.getResultList();
             if (!Listpa.isEmpty()) {
-               
+
                 return false;
             }
 
-            
         } catch (Exception e) {
-            
-            
-            
+
         }
         return true;
-        
+
     }
-    
-     public boolean modificarEquipo(int idEquipo, Pais paisidPais, Zona zonaidZona, Mundial mundialidMundial) {
+
+    public boolean modificarEquipo(int idEquipo, Pais paisidPais, Zona zonaidZona, Mundial mundialidMundial) {
         try {
             Equipo p = em.find(Equipo.class, idEquipo);
             p.setPaisidPais(paisidPais);
@@ -133,64 +122,62 @@ public class EquipoSessionBean {
             return false;
         }
     }
- 
 
-     public List<Equipo> obtenerEquiposOrdenZona()
-     {
-         try {
-             javax.persistence.Query q= em.createNamedQuery("Equipo.findEquipoByZona"); 
-             return q.getResultList();
-         } catch (Exception e) {
-             return null;
-         }
-     }
-
-             public List <String> obtenerfix(){
-
-       
-
- int teams =4;
- 
-   
-    // Generate the schedule using round robin algorithm.
-    int totalRounds = (teams - 1)*2;
-    int matchesPerRound = teams / 2;
-    String[][] rounds = new String[totalRounds][matchesPerRound];
-     List<String> list = null;
-     
-
-    for (int round = 0; round < totalRounds; round++) {
-        for (int match = 0; match < matchesPerRound; match++) {
-            int home = (round + match) % (teams - 1);
-            int away = (teams - match + round -1) % (teams - 1);
-          
-            // Last team stays in the same place while the others
-            // rotate around it.
-            if (match == 0) {
-                away = teams - 1;
-            }
-
-            // Add one so teams are number 1 to teams not 0 to teams - 1
-            // upon display.
-            rounds[round][match] = ((home + 1)+ " Vs " + (away + 1));
-            
-                 list = Arrays.asList(rounds[round]);
-                 
-                }
-       
+    public List<Equipo> obtenerEquiposOrdenZona() {
+        try {
+            javax.persistence.Query q = em.createNamedQuery("Equipo.findEquipoByZona");
+            return q.getResultList();
+        } catch (Exception e) {
+            return null;
+        }
     }
-     
 
-   // Display the rounds    
-//  for (int i = 0; i < rounds.length; i++) {
-       
+    public List<String> obtenerfix() {
 
- 
-     
-       return list;
-     }
-     public void persist(Object object) {
+        int teams = 4;
+
+        // Generate the schedule using round robin algorithm.
+        int totalRounds = (teams - 1) * 2;
+        int matchesPerRound = teams / 2;
+        String[][] rounds = new String[totalRounds][matchesPerRound];
+        List<String> myList = null;
+
+        for (int round = 0; round < totalRounds; round++) {
+            for (int match = 0; match < matchesPerRound; match++) {
+                int home = (round + match) % (teams - 1);
+                int away = (teams - match + round - 1) % (teams - 1);
+
+                // Last team stays in the same place while the others
+                // rotate around it.
+                if (match == 0) {
+                    away = teams - 1;
+                }
+
+                // Add one so teams are number 1 to teams not 0 to teams - 1
+                // upon display.
+                rounds[round][match] = ((home + 1) + " Vs " + (away + 1));
+                
+            }
+        }
+        
+        List<String> singleDArray = new ArrayList<String>();
+               
+                for (String[] array : rounds) {
+                    singleDArray.addAll(Arrays.asList(array));
+                }
+                String[] sd = singleDArray.toArray(new String[singleDArray.size()]);
+
+                myList = Arrays.asList(sd);
+
+//        // Display the rounds    
+//        for (int i = 0; i < rounds.length; i++) {   
+//        }
+//        ;
+        return myList;
+    }
+
+    public void persist(Object object) {
         em.persist(object);
     }
-         
+
 }
